bb4acf8f6b6e9bfe81841afa09bbd575b8833a13
Weiting <boterasuo@gmail.com>
2022-01-09 10:20:51 +0800

    upload readfile4

---
+++ basic/readfile4.js
@@ -0,0 +1,22 @@
+const { readFile } = require("fs/promises");
+
+// async function main(){
+//     let result = await readFile("test.tx", "utf-8"); 
+//     console.log(`這是內建的 promise 版本 ${result}`);
+    
+// }
+
+// main();
+
+(async () => {
+    try {
+        let result = await readFile("test.tx", "utf-8"); 
+        console.log(`這是內建的 promise 版本 ${result}`);
+    } catch (err) {
+        console.error(err);
+    }   
+})()
+
+
+
+
