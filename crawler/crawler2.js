ed7cbff787dd57f4df55386135e830e4c58283fc
Weiting <boterasuo@gmail.com>
2022-01-09 21:51:29 +0800

    update folders & files

---
+++ crawler/crawler2.js
@@ -0,0 +1,17 @@
+// 記得安裝後, 先 require
+const axios = require('axios');
+
+
+  (async() => {
+    try {
+      let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220109&stockNo=2330&_=1641716372516");
+      console.log(response.data);
+
+    } catch (err) {
+      console.error(err);
+
+    }
+
+  })();
+  
+
