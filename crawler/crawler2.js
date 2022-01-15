d0b220d45890863558cfa9a2c21b7356f90a9c9b
Weiting <boterasuo@gmail.com>
2022-01-15 11:50:56 +0800

    upload crawler3

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -4,7 +4,20 @@ const axios = require('axios');
 
   (async() => {
     try {
-      let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220109&stockNo=2330&_=1641716372516");
+      let stockNo = 2330;
+      let queryDate = "20220115"
+
+      // let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${stockNo}`);
+
+      let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+        // 這裡可以放一些設定
+        // params: 放 query string 的參數
+        params: {
+          response: "json",
+          date: queryDate,
+          stockNo,
+        }
+      })
       console.log(response.data);
 
     } catch (err) {
---
+++ crawler/crawler3.js
@@ -0,0 +1,39 @@
+// 記得安裝後, 先 require
+const axios = require('axios');
+const { readFile } = require("fs/promises");
+
+
+  (async() => {
+    try {
+      let result = await readFile("stock.txt", "utf-8"); 
+      console.log(result);
+      try {
+        let stockNo = result;
+        let queryDate = "20220115"
+  
+        // TODO: 從 stock.txt 中讀出檔案代碼
+  
+        let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+          // 這裡可以放一些設定
+          // params: 放 query string 的參數
+          params: {
+            response: "json",
+            date: queryDate,
+            stockNo,
+          }
+        })
+        console.log(response.data);
+  
+      } catch (err) {
+        console.error(err);
+  
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+
+
+  })();
+  
+
